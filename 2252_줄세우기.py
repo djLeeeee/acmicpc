d175c0615f856d445ee60943670652eed4b42e92
이동준 <bomul1128@naver.com>
2022-02-04 22:42:27 +0900

    위상정렬 구현 /  일반적인 방법에 역순임 / 정방향도 구현해보자

---
+++ 2252_줄세우기.py
@@ -0,0 +1,37 @@
+# 2252 줄세우기
+# 위상정렬 맛보기
+
+########################################################
+# 시간초과 코드(수정 완료)
+# 뒤에서 역순으로 진행(뻗어나가는 간선이 없는 노드를 찾음)
+# 매번 connection을 뒤져서 leaves를 갱신하는 것은 별로다.
+# 자신에게서 나가는 간선의 수를 나타낸 get_out 사용
+# get_out이 0인 노드는 leaf일 것이다!
+# deque 기능을 적절히 잘 사용해야 한다... 덱 사용 더 연습해보자.
+
+from sys import stdin as s
+from collections import deque
+
+n, m = map(int, s.readline().split())
+
+connection = [ [ ] for _ in range(n + 1) ]
+get_out = [0] * (n + 1)
+
+for i in range(m):
+    x, y = map(int, s.readline().split())
+    connection[y].append(x)
+    get_out[x] += 1
+
+result = deque([])
+leaves = deque([])
+for i in range(1, n + 1):
+    if get_out[i] == 0:
+        leaves.append(i)
+while leaves:
+    a = leaves.popleft()
+    result.appendleft(a)
+    for j in connection[a]:
+        get_out[j] -= 1
+        if get_out[j] == 0:
+            leaves.append(j)
+print(*result)
