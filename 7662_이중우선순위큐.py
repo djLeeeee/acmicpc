4cff051d91fde2809616037173f8f6b5400f51c4
이동준 <bomul1128@naver.com>
2022-02-06 13:32:22 +0900

    이중 힙 구조 / 연산 처리 여부 검사하는 변수 추가해 해결

--- 7662_이중우선순위큐.py
+++ 7662_이중우선순위큐.py
@@ -3,11 +3,11 @@
 # bottleneck : remove함수 쓴 부분. 최적화 가능?
 # 제거된 원소를 모아놓은 힙 생성? 그 힙 안에 있으면 넘어가기? 는 안 됨 같은 원소 여러개 있을 수 있다
 # nlogn 정렬을 여러번 하기 총 n번 -> remove랑 다를게 없다
-# 
+# 2022/02/06 방문 여부를 체크하는 done 변수 추가
+# 마참내!
 
 import heapq
 from sys import stdin as s
-
 
 N = int(s.readline())
 for _ in range(N):
@@ -15,25 +15,37 @@ for _ in range(N):
 	min_heap = [ ]
 	max_heap = [ ]
 	cnt = 0
-	for _ in range(n):
+	done = [False] * 1000001
+	for i in range(n):
 		order = s.readline().split()
 		if order[0] == 'I':
 			cnt += 1
-			heapq.heappush(min_heap, int(order[1]))
-			heapq.heappush(max_heap, -int(order[1]))
-		elif cnt <= 1:
-			cnt = 0
+			heapq.heappush(min_heap, (int(order[1]), i))
+			heapq.heappush(max_heap, (-int(order[1]), i))
+		elif cnt == 0:
 			min_heap = [ ]
 			max_heap = [ ]
 		else:
 			cnt -= 1
 			if order[1] == '-1':
-				x = heapq.heappop(min_heap)
-				max_heap.remove(-x)
+				while min_heap:
+					x = heapq.heappop(min_heap)
+					if not done[x[1]]:
+						done[x[1]] = True
+						break								
 			else:
-				y = heapq.heappop(max_heap)
-				min_heap.remove(-y)
+				while max_heap:
+					y = heapq.heappop(max_heap)
+					if not done[y[1]]:
+						done[y[1]] = True
+						break				
+	# 마지막 갱신. 아래 4줄이 제일 중요함!
+	while min_heap and done[min_heap[0][1]]:
+		heapq.heappop(min_heap)
+	while max_heap and done[max_heap[0][1]]:
+		heapq.heappop(max_heap)
+	
 	if cnt:
-		print(-heapq.heappop(max_heap), heapq.heappop(min_heap))
+		print(-heapq.heappop(max_heap)[0], heapq.heappop(min_heap)[0])
 	else:
 		print('EMPTY')
