19ba80157411eafd28f53c6d2e2468609f5cfacf
이동준 <bomul1128@naver.com>
2022-02-13 01:26:18 +0900

    메모리 최적화 / old_dp를 없앨 방법 있나?

--- 9251_LCS.py
+++ 9251_LCS.py
@@ -1,19 +1,20 @@
-# DP 알고리즘 / LCS
-# 공통 부분 문자열 최대 길이 출력
-
 from sys import stdin as s
 
-A =  s.readline().strip()
-B =  s.readline().strip()
+A = s.readline().strip()
+B = s.readline().strip()
 
 la = len(A)
-lb= len(B)
-
-dp = [ [0] * (lb + 1) for _ in range(la + 1) ]
-for y in range(1, lb + 1):
-    for x in range(1, la + 1):
+lb = len(B)
+if la < lb:
+    A, B = B, A
+    la, lb = lb, la
+dp = [''] * (lb + 1)
+for x in range(1, la + 1):
+    old_dp = dp[:]
+    for y in range(1, lb + 1):
         if A[x - 1] == B[y - 1]:
-            dp[x][y] = dp[x - 1][y - 1] + 1
+            dp[y] = old_dp[y - 1] + A[x - 1]
         else:
-            dp[x][y] = max(dp[x - 1][y], dp[x][y - 1])
-print(dp[-1][-1])
+            dp[y] = max(dp[y], dp[y - 1], key=lambda n: len(n))
+print(len(dp[-1]))
+# print(dp[-1])
