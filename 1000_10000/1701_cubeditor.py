72a5709bd803cf6c88500cd1da9c17fbbc444ef0
이동준 <bomul1128@naver.com>
2022-02-16 21:11:55 +0900

    KMP 알고리즘 활용 문제 / 오답 코드 수정 필요.

---
+++ 1000_10000/1701_cubeditor.py
@@ -0,0 +1,18 @@
+# 1701 Cube editor
+# 2022-02-16 현재 문제 : 기존 pi 함수는 접두사 접미사만 체크
+# 문제에선 중간에서 시작하는? 접두사도 체크해야함
+# 예시) abbba 답 2 (bb) 현재 1 (a)
+
+from sys import stdin as s
+
+target = s.readline().strip()
+n = len(target)
+pi = [0] * n
+idx = 0
+for i in range(1, n):
+    while idx > 0 and target[idx] != target[i]:
+        idx = pi[idx - 1]
+    if target[idx] == target[i]:
+        idx += 1
+        pi[i] = idx
+print(max(pi))
