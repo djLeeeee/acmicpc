ff327ababca51dfa4f1626c5a03c432353cb26cc
이동준 <bomul1128@naver.com>
2022-02-23 10:59:53 +0900

    백트래킹 활용 문제 풀이 주석 추가

--- 1000_10000/2580_sudoku.py
+++ 1000_10000/2580_sudoku.py
@@ -4,40 +4,43 @@
 
 from sys import stdin as s
 
-def box(x, y, board):
-	result = [ ]
-	for i in range(3):
-		result += board[(x // 3) * 3 + i][(y // 3) * 3: (y // 3) * 3 + 3]
-	return result
 
-def check_sudoku(x, y, board, p):
-	cnt_in = [ ]
-	for j in range(9):
-		if board[j][y] == p or board[x][j] == p:
-			return False
-	if p in box(x, y, board):
-		return False
-	return True
+def box(x, y, board):  # 해당 위치가 포함된 박스를 반환
+    result = []
+    for i in range(3):
+        result += board[(x // 3) * 3 + i][(y // 3) * 3: (y // 3) * 3 + 3]
+    return result
 
-def put_in(n = 0):
-	if n == len(empty):
-		for i in range(9):
-			print(*sudoku[i])
-		exit(0)
-	x = empty[n][0]
-	y = empty[n][1]
-	for j in range(1, 10):
-		if check_sudoku(x, y, sudoku, j):
-			sudoku[x][y] = j
-			put_in(n + 1)
-			sudoku[x][y] = 0
 
-sudoku = [ ]
+def check_sudoku(x, y, board, p):  # 해당 위치에 p를 넣을 수 있나 체크
+    for j in range(9):
+        if board[j][y] == p or board[x][j] == p:
+            return False
+    if p in box(x, y, board):
+        return False
+    return True
+
+
+def put_in(n=0):
+    if n == len(empty):             # 다 채웠다면 (빈 칸의 수만큼 채웠다면)
+        for i in range(9):          # 출력양식에 맞게 출력하고
+            print(*sudoku[i])
+        exit(0)                     # 그대로 종료
+    x = empty[n][0]                 # 아직 덜 채웠으면 빈 칸 좌표 받고
+    y = empty[n][1]
+    for j in range(1, 10):          # 그 칸에 1부터 9까지 들어갈 수 있는지 체크
+        if check_sudoku(x, y, sudoku, j):
+            sudoku[x][y] = j        # 해당 칸에 넣을 수 있는 수 넣고
+            put_in(n + 1)           # 다음 칸을 넣으러 가자
+            sudoku[x][y] = 0        # 코드 종료가 안 됐다면 답이 아니므로 초기화
+
+
+sudoku = []
 for _ in range(9):
-	sudoku.append(list(map(int, s.readline().split())))
-empty = [ ]
-for i in range(9):
-	for j in range(9):
-		if sudoku[i][j] == 0:
-			empty.append([i, j])
+    sudoku.append(list(map(int, s.readline().split())))
+empty = []
+for r in range(9):
+    for c in range(9):
+        if sudoku[r][c] == 0:
+            empty.append([r, c])
 put_in()
