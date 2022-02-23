27d6f87c601c0932f61a87bac5cd5e63d00370eb
이동준 <bomul1128@naver.com>
2022-02-23 10:59:59 +0900

    백트래킹 활용 문제 풀이 주석 추가

--- 1000_10000/9663_Nqueens.py
+++ 1000_10000/9663_Nqueens.py
@@ -2,28 +2,26 @@
 # 백트래킹 사용
 
 def can_go(x, queen):
-	if x in queen:
-		return False
-	l = len(queen)
-	for n, q in enumerate(queen):
-		if abs(q - x) == l - n:
-			return False
-	return True
+    # 세로 체크
+    if x in queen:
+        return False
+    l = len(queen)
+    # 대각선 체크
+    for n, q in enumerate(queen):
+        if abs(q - x) == l - n:
+            return False
+    return True
+
 
 def n_queens(n, queen):
-	global result
-	# if len(queen) == n:
-	# 	result += 1
-	# else:
-	# 	for i in range(n):
-	# 		if can_go(i, queen):
-	# 			n_queens(n, queen + [i])
-	for i in range(n):
-		if can_go(i, queen):
-			if len(queen) == n - 1:
-				result += 1
-			else:
-				n_queens(n, queen + [i])
+    global result
+    for i in range(n):
+        if can_go(i, queen):                # i 번째에 퀸 배치 가능한가?
+            if len(queen) == n - 1:         # 그 퀸이 마지막 퀸인가?
+                result += 1                 # 그렇다면 결과 + 1
+            else:
+                n_queens(n, queen + [i])    # 아직 안 끝났다면 더 진행해라.
+
 
 result = 0
 n_queens(15, [])
