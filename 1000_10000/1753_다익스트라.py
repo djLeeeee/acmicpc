7a549be0edc533925843a8e6d56b9943f5b301c4
이동준 <bomul1128@naver.com>
2022-02-24 15:09:19 +0900

    어떻게 사람 이름이 다익스트라 ㅋㅋ

--- 1000_10000/1753_다익스트라.py
+++ 1000_10000/1753_다익스트라.py
@@ -3,27 +3,28 @@
 # 그냥 플로이드 와샬 쓰는게 낫지 않나? => 플로이드 와샬은 모든 점에 대해서 진행해야하네...
 
 from sys import stdin as s
-import heapq
 from math import inf
+import heapq
 
 v, e = map(int, s.readline().split())
 init = int(s.readline())
-connection = [[] * (v + 1) for _ in range(v + 1)]
+connection = [[] for _ in range(v + 1)]
+for _ in range(e):
+    x, y, z = map(int, s.readline().split())
+    connection[x].append((z, y))
 d = [inf] * (v + 1)
 d[init] = 0
-for _ in range(e):
-    x, y, m = map(int, s.readline().split())
-    connection[x].append((y, m))
-
 heap = []
 heapq.heappush(heap, (0, init))
 while heap:
-    now = heapq.heappop(heap)  # 갱신된 노드 중 가장 가까운 놈 / 이걸 위해 힙구조 사용
-    for i in connection[now[1]]:
-        if i[1] + now[0] < d[i[0]]:
-            d[i[0]] = i[1] + now[0]
-            heapq.heappush(heap, (d[i[0]], i[0]))
-
+    # 갱신된 노드 중 가장 가까운 놈 / 이걸 위해 힙구조 사용
+    distance, now = heapq.heappop(heap)
+    if d[now] < distance:
+        continue
+    for cd, cy in connection[now]:
+        if d[cy] > d[now] + cd:
+            d[cy] = d[now] + cd
+            heapq.heappush(heap, (d[cy], cy))
 for i in range(1, v + 1):
     if d[i] == inf:
         print("INF")
