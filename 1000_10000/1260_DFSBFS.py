9b6eecdd87c6165b813788df4e2f5de3127289da
이동준 <bomul1128@naver.com>
2022-02-25 21:20:31 +0900

    코드 업데이트

--- 1000_10000/1260_DFSBFS.py
+++ 1000_10000/1260_DFSBFS.py
@@ -1,72 +1,42 @@
 from sys import stdin as s
+from collections import deque
 
-# def DFS(line, start): # 깊이 우선 (1)
-#     result = [ ]
-#     need_visit = [start]
-#     while need_visit:
-#         x= need_visit.pop()
-#         if x not in result:
-#             result.append(x)
-#             for i in range(len(line) - 1, 0, -1):
-#                 if line[x][i] == 1:
-#                     need_visit.append(i)
-#     return result
 
-def DFS(line, start): # 깊이 우선 (2)
-    result = [ ]
+def DFS(start):  # 깊이 우선 (2)
+    result = []
     need_visit = [start]
     while need_visit:
-        x= need_visit.pop()
-        if x not in result:
-            result.append(x)
-            for i in line[x][::-1]:
+        now = need_visit.pop()
+        if now not in result:
+            result.append(now)
+            for i in connection[now][::-1]:
                 need_visit.append(i)
     return result
 
-# def BFS(line, start): # 너비 우선 (1)
-#     result = [start]
-#     start_points = result
-#     while True:
-#         new_visit = []
-#         for i in start_points:
-#             for j in range(len(line[i])):
-#                 if line[i][j] == 1 and j not in result and j not in new_visit:
-#                     new_visit.append(j)
-#         result += new_visit
-#         start_points = new_visit
-#         if new_visit == []:
-#             break
-#     return result
 
-def BFS(line, start): # 너비 우선 (2)
-    result = [start]
-    start_points = result
-    while start_points:
-        new_visit = []
-        for i in start_points:
-            for j in line[i]:
-                if j not in result and j not in new_visit:
-                    new_visit.append(j)
-        result += new_visit
-        start_points = new_visit
+def BFS(start):  # 너비 우선 (2)
+    result = []
+    need_visit = deque([start])
+    while need_visit:
+        now = need_visit.popleft()
+        if now in result:
+            continue
+        result.append(now)
+        for can_go in connection[now]:
+            if can_go not in result:
+                need_visit.append(can_go)
     return result
 
-N, M, V = map( int, s.readline().split() ) # 변수 받기
 
-# line = [ [0] * (N + 1) for _ in range(N + 1) ]  # line graph 생성 (1)
-# for _ in range(M):
-#     x, y = map( int, s.readline().split() )
-#     line[x][y] = 1
-#     line[y][x] = 1
-
-line = [ [] for i in range(N + 1) ] # line graph 생성 (2)
+N, M, V = map(int, s.readline().split())  # 변수 받기
+connection = [[] for i in range(N + 1)]  # line graph 생성 (2)
 for _ in range(M):
-    x, y = map( int, s.readline().split() )
-    line[x].append(y)
-    line[y].append(x)
+    x, y = map(int, s.readline().split())
+    connection[x].append(y)
+    connection[y].append(x)
 
-for i in range(N+1):
-    line[i].sort()
+for i in range(N + 1):
+    connection[i].sort()
 
-print(*DFS(line, V))
-print(*BFS(line, V))
+print(*DFS(V))
+print(*BFS(V))
