cb9530e1410842a1294c38cb964070c554e097f5
이동준 <bomul1128@naver.com>
2022-03-01 18:36:23 +0900

    코드 변수를 더 직관성 있게 수정

--- 10001_29999/17472_다리만들기2.py
+++ 10001_29999/17472_다리만들기2.py
@@ -27,42 +27,42 @@ def dfs(a: int, b: int) -> None:
     for d in range(4):
         na = a + dx[d]
         nb = b + dy[d]
-        if 0 <= na < n and 0 <= nb < m and board[na][nb] == 1:
+        if 0 <= na < n and 0 <= nb < m and board[na][nb] == '1':
             dfs(na, nb)
 
 
 n, m = map(int, input().split())
 board = []
-idx = 1
+idx = 0
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-board = [list(map(int, input().split())) for _ in range(n)]
+board = [input().rstrip().split() for _ in range(n)]
 for x in range(n):
     for y in range(m):
-        if board[x][y] == 1:
+        if board[x][y] == '1':
             idx += 1
             dfs(x, y)
 p = list(range(idx + 1))
 bridge = set()
 for xx in range(n):
     for yy in range(m):
-        if board[xx][yy]:
+        if board[xx][yy] != '0':
             for dd in range(4):
                 nx, ny = xx, yy
                 cost = 0
                 nx += dx[dd]
                 ny += dy[dd]
-                while 0 <= nx < n and 0 <= ny < m and not board[nx][ny]:
+                while 0 <= nx < n and 0 <= ny < m and board[nx][ny] == '0':
                     cost += 1
                     nx += dx[dd]
                     ny += dy[dd]
-                if 0 <= nx < n and 0 <= ny < m and board[nx][ny] and cost > 1:
+                if 0 <= nx < n and 0 <= ny < m and board[nx][ny] != '0' and cost > 1:
                     bridge.add((cost, board[xx][yy], board[nx][ny]))
 bridge = list(bridge)
 heapq.heapify(bridge)
 edge = 0
 total = 0
-while edge < idx - 2 and bridge:
+while edge < idx - 1 and bridge:
     c, init, fin = heapq.heappop(bridge)
     if find(init) != find(fin):
         union(init, fin)
