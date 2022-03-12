b863c264a0ea433b98f0f8b9fa6ca1af5862f800
이동준 <bomul1128@naver.com>
2022-03-13 00:43:57 +0900

    dp 알고리즘 / 컨벤션 수정

--- 1000_10000/1463_1로만들기_DP알고리즘.py
+++ 1000_10000/1463_1로만들기_DP알고리즘.py
@@ -1,25 +1,25 @@
-a=int(input())
-x=[0 for i in range(a+1)]
-x[a]=1
-n=1
-updated=[a]
+a = int(input())
+x = [0 for i in range(a + 1)]
+x[a] = 1
+n = 1
+updated = [a]
 while True:
     if 1 in updated:
         break
-    reupdated=[]
+    new_updated = []
     for i in updated:
-        if i%2==0 and x[i//2]==0:
-            x[i//2]=n
-            reupdated.append(i//2)
-        if i%3==0 and x[i//3]==0:
-            x[i//3]=n
-            reupdated.append(i//3)
-        if x[i-1]==0:
-            x[i-1]=n
-            reupdated.append(i-1)
-    updated=reupdated
-    n+=1
-if a==1:
+        if i % 2 == 0 and x[i // 2] == 0:
+            x[i // 2] = n
+            new_updated.append(i // 2)
+        if i % 3 == 0 and x[i // 3] == 0:
+            x[i // 3] = n
+            new_updated.append(i // 3)
+        if x[i - 1] == 0:
+            x[i - 1] = n
+            new_updated.append(i - 1)
+    updated = new_updated
+    n += 1
+if a == 1:
     print(0)
 else:
     print(x[1])
