15b71b56b6750afd884b2ea9ab92a330db973a9a
이동준 <bomul1128@naver.com>
2022-03-13 20:20:28 +0900

    최소 스패닝 트리 느낌으로 접근 / 컨벤션 수정

--- 1000_10000/6091_핑크플로이드.py
+++ 1000_10000/6091_핑크플로이드.py
@@ -4,17 +4,20 @@
 
 from sys import stdin as s
 from sys import setrecursionlimit as strc
+
 strc(10 ** 6)
 
 n = int(s.readline())
 
 parent = list(range(n + 1))
 
+
 def find(target):
-	if target == parent[target]:
-		return target
-	parent[target] = find(parent[target])
-	return parent[target]
+    if target == parent[target]:
+        return target
+    parent[target] = find(parent[target])
+    return parent[target]
+
 
 def union(a, b):
     a = find(a)
@@ -24,23 +27,23 @@ def union(a, b):
     else:
         parent[a] = b
 
-connection = [ ]
+
+connection = []
 for i in range(1, n):
     a = list(map(int, s.readline().split()))
     for x, j in enumerate(a):
         connection.append([j, i, i + x + 1])
-
-connection.sort(reverse = True)
-
+connection.sort()
 v = 0
-result = [ [] * (n + 1) for _ in range(n + 1) ]
+result = [[] * (n + 1) for _ in range(n + 1)]
+pointer = 0
 while v < n - 1:
-    line = connection.pop()
+    line = connection[pointer]
     if find(line[1]) != find(line[2]):
         union(line[1], line[2])
         result[line[1]].append(line[2])
         result[line[2]].append(line[1])
         v += 1
-
+    pointer += 1
 for i in range(1, n + 1):
     print(len(result[i]), *sorted(result[i]))
