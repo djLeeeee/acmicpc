7f07186bfc856c6b49bec599116fb2f730fc61f7
이동준 <bomul1128@naver.com>
2022-03-19 18:24:31 +0900

    kmp

--- 1000_10000/1701_cubeditor.py
+++ 1000_10000/1701_cubeditor.py
@@ -5,14 +5,23 @@
 
 from sys import stdin as s
 
-target = s.readline().strip()
-n = len(target)
-pi = [0] * n
-idx = 0
-for i in range(1, n):
-    while idx > 0 and target[idx] != target[i]:
-        idx = pi[idx - 1]
-    if target[idx] == target[i]:
-        idx += 1
-        pi[i] = idx
-print(max(pi))
+
+def max_fix(target):
+    n = len(target)
+    pi = [0] * n
+    idx = 0
+    for i in range(1, n):
+        while idx > 0 and target[idx] != target[i]:
+            idx = pi[idx - 1]
+        if target[idx] == target[i]:
+            idx += 1
+            pi[i] = idx
+    return max(pi)
+
+
+editor = s.readline().strip()
+length = len(editor)
+ans = 0
+for j in range(length - 1):
+    ans = max(ans, max_fix(editor[j:]))
+print(ans)
