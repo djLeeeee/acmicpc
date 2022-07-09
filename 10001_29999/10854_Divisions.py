c4aefe0a3a92a4bc35616bb052be20286687e72e
이동준 <bomul1128@naver.com>
2022-07-10 07:05:43 +0900

    폴라드 로, 밀러 라빈 / 소인수분해를 n^0.25에 처리하는 무친 알고리즘이 있다고?!

---
+++ 10001_29999/10854_Divisions.py
@@ -0,0 +1,71 @@
+from random import randrange
+from math import gcd
+from collections import defaultdict
+
+
+def power(a, b, mod):
+    result = 1
+    while b > 0:
+        if b % 2:
+            result = (result * a) % mod
+        a = (a * a) % mod
+        b //= 2
+    return result
+
+
+def Miller_Rabin(num, check):
+    if num == check:
+        return 1
+    k = num - 1
+    while True:
+        x = power(check, k, num)
+        if x == num - 1:
+            return 1
+        if k % 2:
+            if x == 1:
+                return 1
+            break
+        k //= 2
+    return 0
+
+
+def is_prime(t):
+    checker = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    for c in checker:
+        if not Miller_Rabin(t, c):
+            return 0
+    return 1
+
+
+def pollard_rho(t):
+    if t == 1:
+        return
+    if not t % 2:
+        div[2] += 1
+        pollard_rho(t // 2)
+        return
+    if is_prime(t):
+        div[t] += 1
+        return
+    x = randrange(2, t)
+    y = x
+    c = randrange(1, 10)
+    g = 1
+    while g == 1:
+        x = (x * x % t + c)
+        y = (y * y % t + c)
+        y = (y * y % t + c)
+        g = gcd(x - y, t)
+        if g == t:
+            return pollard_rho(t)
+    pollard_rho(g)
+    pollard_rho(t // g)
+
+
+target = int(input())
+div = defaultdict(int)
+pollard_rho(target)
+ans = 1
+for v in div.values():
+    ans *= (v + 1)
+print(ans)
